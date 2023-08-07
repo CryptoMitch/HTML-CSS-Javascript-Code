aba60eec07aff5e323e16e59f087068da4dc9ee3
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-07 15:51:14 +0800

    Crypto Heat Map Project

---
+++ Projects/cryptoHeatMap.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Coin360Widget = () => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <iframe
+      src="https://coin360.com/widget/map?utm_source=embed_map"
+      title="Coin360.com: Cryptocurrency Market State"
+      width="80%"
+      height="700px"
+    ></iframe>
+  </div>
+);
+
+export default Coin360Widget;
